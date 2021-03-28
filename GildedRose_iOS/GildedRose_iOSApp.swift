566504c42141cc06afde2443f11719f2e60866bc
Bill Allen <BillAllenEmail@gmail.com>
2021-03-28 17:32:39 -0500

    Initial Commit

---
+++ GildedRose_iOS/GildedRose_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GildedRose_iOSApp.swift
+//  GildedRose_iOS
+//
+//  Created by Bill Allen on 3/28/21.
+//
+
+import SwiftUI
+
+@main
+struct GildedRose_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
